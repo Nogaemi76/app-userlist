0df00fc5ccfa25837e3bf837e18aefd877b55b9d
Emilie <milogare@yahoo.fr>
2022-06-23 18:38:50 +0200

    Added validation & reset

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -6,7 +6,15 @@ import classes from "./AddUser.module.css"
 const AddUser = props => {
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    };
     console.log(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
   };
 
   const [enteredUsername, setEnteredUsername] = useState('');
@@ -27,10 +35,16 @@ const AddUser = props => {
         <input
           id="username"
           type="text"
+          value={enteredUsername}
           onChange={usernameChangeHandler}
         ></input>
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler}></input>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        ></input>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
